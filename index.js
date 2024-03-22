f18dc4d9468ec792bc1816dfd11d690535d0224e
mendarrr <sarahabby541@gmail.com>
2024-03-22 21:01:08 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+console.log( multiply )
+
+var random = Math.floor(Math.random() * 100) + 1;
+console.log(random); //imekataa fikiria for us
+
+const num3 = 29
+const num4 = 5
+const mod = num3 % num4
+mod === 4
+console.log( mod )
+
+const max = Math.max(10, 15, 20)
+console.log( max )
